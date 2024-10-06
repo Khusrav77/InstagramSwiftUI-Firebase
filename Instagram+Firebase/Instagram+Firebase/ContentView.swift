951ac09cbc0eec62847a8f75202076533372953a
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-10-06 22:30:40 +0300

    Created Xcode project

---
+++ Instagram+Firebase/Instagram+Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagram+Firebase
+//
+//  Created by Khusrav Safiev on 10/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagram+Firebase/Instagram+Firebase/Instagram_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_FirebaseApp.swift
+//  Instagram+Firebase
+//
+//  Created by Khusrav Safiev on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
