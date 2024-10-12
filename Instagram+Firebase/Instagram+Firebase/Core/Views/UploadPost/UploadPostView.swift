2b5f886d3858a135039014b7809787b6e1de999d
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-10-12 20:47:37 +0300

    added UploadPostView

--- Instagram+Firebase/Instagram+Firebase/Core/Views/UploadPost/UploadPostView.swift
+++ Instagram+Firebase/Instagram+Firebase/Core/Views/UploadPost/UploadPostView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    // MARK: - Properties
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var capionsText: String = ""
+    
+    // MARK: - Body
     var body: some View {
-        Text("UploadPostView!")
+        VStack {
+            if postImage == nil {
+                Button {
+                    
+                }label: {
+                    Image("plus_photo")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180 , height: 180)
+                        .padding(.top, 56)
+                        .clipped()
+                        .foregroundStyle(.black)
+                }
+            } else {
+                HStack(alignment: .top) {
+                    Image(systemName: "person")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96 , height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption", text: $capionsText)
+                        .padding()
+                        .background(Color.white.opacity(0.1))
+                        .cornerRadius(10)
+                }.padding()
+                
+                Button {
+                    
+                } label: {
+                    Text("Share")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+
+            }
+            Spacer()
+        }
     }
 }
 
